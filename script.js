91cda0ef7da2deef28d972ce49fc8948bcadb226
Federico Stillitano <federico.stillitano@ibm.com>
2022-02-15 21:32:34 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Main function which calculates the output based on user inputs
 function compute() {
 	var principal = document.getElementById("principal").value;
 	var biggerThanZero = parseInt(principal) > 0;
+    // Check whether 'Amount' input is zero or negative, in which case it resets inputs and brings focus back to 'Amount' field
 	if (!biggerThanZero) {
 		alert("Enter a positive number");
 		document.getElementById("principal").focus();
@@ -10,10 +12,13 @@ function compute() {
 	var years = document.getElementById("years").value;
 	var interest = principal * years * rate / 100;
 	var year = new Date().getFullYear() + parseInt(years);
+    //Results
 	document.getElementById("result").innerHTML = "If you deposit <mark>£" + principal + "</mark>,\<br\>at an interest rate of <mark>" + rate + "%</mark>\<br\>You will receive an amount of <mark>£" + interest + "</mark>,\<br\>in the year <mark>" + year + "</mark>\<br\>";
-	document.getElementById('principal').focus();
+	//Set focus back to 'Amount' field
+    document.getElementById('principal').focus();
 }
 
+// Function to update the 'rate' label after the user releases the range slider
 function updateRate() {
 	var rateval = document.getElementById("rate").value;
 	document.getElementById("rate_val").innerText = rateval;
